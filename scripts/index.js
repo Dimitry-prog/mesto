c9aaf5e11954adb98ba8889aa6ee1d109b7745d8
Dmitry <kfifa3@gmail.com>
2022-06-27 12:03:33 +0300

    fix: res at 320px, rename const

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,36 @@
-const edit = document.querySelector('.profile__edit');
+const editButton = document.querySelector('.profile__edit');
 const popUp = document.querySelector('.pop-up');
-const name = document.querySelector('.profile__name');
-const activity = document.querySelector('.profile__activity');
+const profileName = document.querySelector('.profile__name');
+const profileActivity = document.querySelector('.profile__activity');
 const editName = document.querySelector('.form__input_type_name');
 const editActivity = document.querySelector('.form__input_type_activity');
-const save = document.querySelector('.form__input_type_save');
-const page = document.querySelector('.page');
-const close = document.querySelector('.pop-up__close');
+const saveButton = document.querySelector('.form__input_type_save');
+const pageElement = document.querySelector('.page');
+const closePopUpButton = document.querySelector('.pop-up__close');
 
+function closePopUp() {
+  popUp.classList.remove('pop-up_opened');
+  pageElement.classList.remove('page_type_hidden');
+}
 
-edit.addEventListener('click', function() {
-  popUp.classList.add('pop-up_type_show');
-  page.classList.add('page_type_hidden');
-  editName.value = name.textContent;
-  editActivity.value = activity.textContent;
+editButton.addEventListener('click', function() {
+  popUp.classList.add('pop-up_opened');
+  pageElement.classList.add('page_type_hidden');
+  editName.value = profileName.textContent;
+  editActivity.value = profileActivity.textContent;
 });
 
-window.addEventListener('click', function(event) {
-  if (event.target === popUp) {
-    popUp.classList.remove('pop-up_type_show');
-    page.classList.remove('page_type_hidden');
-  }
-});
-
-save.addEventListener('click', function(event) {
+saveButton.addEventListener('click', function(event) {
   event.preventDefault();
-  name.textContent = editName.value;
-  activity.textContent = editActivity.value;
-  popUp.classList.remove('pop-up_type_show');
-  page.classList.remove('page_type_hidden');
+  profileName.textContent = editName.value;
+  profileActivity.textContent = editActivity.value;
+  closePopUp();
 });
 
-close.addEventListener('click', function() {
-  popUp.classList.remove('pop-up_type_show');
-  page.classList.remove('page_type_hidden');
+closePopUpButton.addEventListener('click', closePopUp);
+
+popUp.addEventListener('click', function(event) {
+  if (event.target === popUp) {
+    closePopUp();
+  }
 });
