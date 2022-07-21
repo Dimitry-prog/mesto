d02454a097329c7b7add565f9c8b9efc0158797a
Dmitry <kfifa3@gmail.com>
2022-07-21 17:03:55 +0300

    fix: required fields

---
+++ scripts/validate.js
@@ -0,0 +1,34 @@
+/* const profileEditButton = document.querySelector('.profile__edit');
+const popUpProfile = document.querySelector('.pop-up_profile');
+const popUpOverlay = document.querySelector('.pop-up__overlay');
+const profileName = document.querySelector('.profile__name');
+const profileActivity = document.querySelector('.profile__activity');
+const pageElement = document.querySelector('.page');
+const popUpCard = document.querySelector('.pop-up_card');
+const cardAddButton = document.querySelector('.profile__add');
+const popUpImg = document.querySelector('.pop-up_img');
+const profilePopUpClose = popUpProfile.querySelector('.button_type_close-profile');
+const popUpProfileForm = popUpProfile.querySelector('.form_type_profile');
+const nameInput = popUpProfileForm.querySelector('.form__input_type_name');
+const activityInput = popUpProfileForm.querySelector('.form__input_type_activity');
+const popUpCardForm = popUpCard.querySelector('.form_type_card');
+const popUpCardClose = popUpCard.querySelector('.button_type_close-card');
+const placeInput = popUpCardForm.querySelector('.form__input_type_place');
+const linkInput = popUpCardForm.querySelector('.form__input_type_link');
+const popUpImgClose = popUpImg.querySelector('.button_type_close-img');
+const popUpText = popUpImg.querySelector('.pop-up__text');
+const popUpFiqure = popUpImg.querySelector('.pop-up__fiqure');
+const popUpPicture = popUpImg.querySelector('.pop-up__picture');
+const templateCard = document.querySelector('#template-card').content;
+const elementsList = document.querySelector('.elements__list');
+
+
+enableValidation({
+  formSelector: '.popup__form',
+  submitButtonSelector: '.popup__button',
+  inputSelector: '.popup__input',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+ */
