5dba77ef6a8ba105be32db54a7b9b830a00be872
Dimitry-prog <kfifa3@gmail.com>
2022-09-08 21:53:34 +0300

    added: getResponse to api.js

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -2,8 +2,13 @@ export default class Api {
   constructor({ url, headers }) {
     this.url = url;
     this._headers = headers;
-    console.log(this.url);
-    console.log(this._headers);
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
   }
 
   getUsersInfo() {
@@ -11,11 +16,7 @@ export default class Api {
       headers: this._headers,
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -24,11 +25,7 @@ export default class Api {
       headers: this._headers,
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -42,11 +39,7 @@ export default class Api {
       }),
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -60,11 +53,7 @@ export default class Api {
       }),
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -73,11 +62,7 @@ export default class Api {
       headers: this._headers
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -87,11 +72,7 @@ export default class Api {
       headers: this._headers,
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -101,11 +82,7 @@ export default class Api {
       headers: this._headers,
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -115,11 +92,7 @@ export default class Api {
       headers: this._headers,
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
@@ -132,11 +105,7 @@ export default class Api {
       }),
     })
       .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject(`Ошибка: ${res.status}`);
+        return this._getResponseData(res);
       });
   }
 
